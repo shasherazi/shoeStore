2f3e0339f51d457761f6ee45cf08721a7bb50162
shasherazi <hassanrandomz@gmail.com>
2023-08-30 18:03:09 +0500

    Finished hero section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,12 @@
-import { useEffect, useState } from 'react'
 import './App.css'
+import { Hero } from './components/Hero'
+import { Nav } from './components/Nav'
 
 function App() {
-  const [res, setRes] = useState("");
-
-  useEffect(() => {
-    fetch('http://localhost:6969/shoes')
-      .then(res => res.json())
-      .then(data => setRes(data.message))
-  }, [])
-
   return (
     <>
-      <h1 className='text-4xl font-bold'> {res} </h1>
+      <Nav />
+      <Hero />
     </>
   )
 }
---
+++ frontend/src/components/Nav.tsx
@@ -0,0 +1,22 @@
+import { AiOutlineHeart, AiOutlineSearch, AiOutlineShoppingCart, AiOutlineUser } from 'react-icons/ai'
+
+export const Nav = () => {
+  return (
+    <div className="div font-inter border-b border-gray-300 text-2xl text-gray-500 flex justify-between items-center py-5 px-4">
+      <div className="left flex gap-4">
+        <AiOutlineHeart />
+        <AiOutlineSearch />
+      </div>
+
+      <div className="center flex text-xl gap-4">
+        <p> Home </p>
+        <p> About </p>
+      </div>
+
+      <div className="right flex gap-4">
+        <AiOutlineUser />
+        <AiOutlineShoppingCart />
+      </div>
+    </div>
+  )
+}
