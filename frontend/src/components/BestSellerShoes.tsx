77deaf2c48ba549c8ac86117869595d541040050
shasherazi <hassanrandomz@gmail.com>
2023-09-01 00:04:28 +0500

    Changed backend to mongodb atlas

--- frontend/src/components/BestSellerShoes.tsx
+++ frontend/src/components/BestSellerShoes.tsx
@@ -1,53 +1,44 @@
+import { useEffect, useState } from "react";
 import { BestSellerShoe } from "./BestSellerShoe";
 
 export const BestSellerShoes = () => {
+  const [bestSellerShoes, setBestSellerShoes] = useState([]);
+
+  const fetchBestSellerShoes = async () => {
+    const res = await fetch("http://localhost:6969/shoes");
+    const data = await res.json();
+    const bestSellerShoes = data.map((shoe: any) => {
+      return {
+        name: shoe.name,
+        img: shoe.image,
+        price: shoe.price,
+        discount: shoe.discount,
+      };
+    });
+    setBestSellerShoes(bestSellerShoes);
+  };
+
+  useEffect(() => {
+    fetchBestSellerShoes();
+  }, []);
+
   return (
     <div className="best-seller-shoes flex flex-col items-center mt-24 mb-10">
       <h1 className="text-4xl font-playfair text-[#331800]">Best Sellers</h1>
 
       <div className="mt-10 grid w-full justify-items-center grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 px-10 gap-5">
-        <BestSellerShoe
-          name="Cole Haan Original Grand Wingtip Oxfords, Cognac"
-          img="https://image.menswearhouse.com/is/image/TMW/MW40_40ZC_05_COLE_HAAN_COGNAC_IVORY_MAIN?$40GridDesktop$"
-          price={69.99}
-        />
-
-
-        <BestSellerShoe
-          name="Florsheim Capitol Plain Toe Canvas Oxfords, Gray"
-          img="https://image.menswearhouse.com/is/image/TMW/MW40_424D_04_JOSEPH_ABBOUD_GRAY_MAIN?$40GridDesktop$"
-          price={69.99}
-        />
-
-
-        <BestSellerShoe
-          name="Stacy Adams Bayne Double Monk Strap Dress Shoes, Black"
-          img="https://image.menswearhouse.com/is/image/TMW/MW40_41RH_02_STACY_ADAMS_BLACK_MAIN?$40GridDesktop$"
-          price={69.99}
-        />
-
-
-        <BestSellerShoe
-          name="Cole Haan Zerogrand Stitchlite Knit Wingtip Oxfords, Navy"
-          img="https://image.menswearhouse.com/is/image/TMW/MW40_41CX_01_COLE_HAAN_NAVY_MAIN?$40GridDesktop$"
-          price={69.99}
-        />
-
-
-        <BestSellerShoe
-          name="Florsheim Kierland Moc Toe Venetian Loafers, Cognac"
-          img="https://image.menswearhouse.com/is/image/TMW/MW40_41M1_05_FLORSHEIM_COGNAC_MAIN?$40GridDesktop$"
-          price={69.99}
-        />
-
-
-        <BestSellerShoe
-          name="Johnston & Murphy Everett Suede Loafers, Black"
-          img="https://image.menswearhouse.com/is/image/TMW/MW40_41R5_02_J_MURPHY_BLACK_MAIN?$40GridDesktop$"
-          price={69.99}
-        />
+        {bestSellerShoes.map((shoe: any, index: number) => {
+          return (
+            <BestSellerShoe
+              name={shoe.name}
+              img={shoe.img}
+              price={shoe.price}
+              discount={shoe.discount}
+              key={index}
+            />
+          );
+        })}
       </div>
     </div>
   );
 };
-
