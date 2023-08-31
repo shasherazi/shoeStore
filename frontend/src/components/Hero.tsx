470b6e4cea740f5c3e04f7826fd7cdebd5799776
shasherazi <hassanrandomz@gmail.com>
2023-08-31 21:57:52 +0500

    Added mobile design for hero section

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,20 +1,20 @@
 export const Hero = () => {
   return (
-    <div className="hero flex gap-5 items-center h-[calc(100vh-69px)]">
+    <div className="hero flex flex-col md:flex-row pb-7 md:p-16 gap-5 items-center md:h-[calc(100vh-69px)]">
       <div className="img flex-1 bg-[#33180010] h-full flex items-center">
         <img
           src="https://d1fufvy4xao6k9.cloudfront.net/feed/img/man_shoe/810870/side.png"
-          className="min-w-0"
+          className="min-w-0 p-5"
         />
       </div>
 
       <div className="text flex-1  text-[#331800]">
-        <h1 className="text-[6.5vw] leading-none font-playfair">
+        <h1 className="text-[10vw] md:text-[6.5vw] leading-none font-playfair">
           Indulge <br /> in the lasting <br />{" "}
           <span className="italic">Look Of Luxury</span>
         </h1>
 
-        <button className="font-inter text-[1.25vw] bg-[#331800] hover:bg-[#4d2400] text-white px-8 py-4 mt-8">
+        <button className="font-inter text-[4wv] md:text-[3vw] lg:text-[1.25vw] bg-[#331800] hover:bg-[#4d2400] text-white px-8 py-4 mt-8">
           See Product
         </button>
       </div>
