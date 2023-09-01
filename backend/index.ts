7606e9d34cd150bde67e8d28847b2b98895dd194
shasherazi <hassanrandomz@gmail.com>
2023-09-01 19:09:04 +0500

    Changed build command

--- backend/index.ts
+++ backend/index.ts
@@ -1,26 +1,86 @@
-import express from 'express';
+import express from "express";
 import cors from "cors";
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import { shoesRouter } from './routes/shoes';
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { shoesRouter } from "./routes/shoes";
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 app.use((req, res, next) => {
-  console.log(`${req.method} ${req.path} ${new Date().toLocaleTimeString('en-US', { hour12: false })}`);
+  console.log(
+    `${req.method} ${req.path} ${new Date().toLocaleTimeString("en-US", {
+      hour12: false,
+    })}`,
+  );
   next();
 });
-app.use('/shoes', shoesRouter);
+app.use("/shoes", shoesRouter);
 
-mongoose.connect(process.env.MONGO_URI || '')
+const html = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hello from Render!</title>
+    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
+    <script>
+      setTimeout(() => {
+        confetti({
+          particleCount: 100,
+          spread: 70,
+          origin: { y: 0.6 },
+          disableForReducedMotion: true
+        });
+      }, 500);
+    </script>
+    <style>
+      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
+      @font-face {
+        font-family: "neo-sans";
+        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
+        font-style: normal;
+        font-weight: 700;
+      }
+      html {
+        font-family: neo-sans;
+        font-weight: 700;
+        font-size: calc(62rem / 16);
+      }
+      body {
+        background: white;
+      }
+      section {
+        border-radius: 1em;
+        padding: 1em;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-right: -50%;
+        transform: translate(-50%, -50%);
+      }
+    </style>
+  </head>
+  <body>
+    <section>
+      Hello from Render!
+    </section>
+  </body>
+</html>
+`;
+
+app.get("/", (req, res) => res.type("html").send(html));
+
+mongoose
+  .connect(process.env.MONGO_URI || "")
   .then(() => {
-    app.listen(port, () => {
+    const server = app.listen(port, () => {
       console.log(`App listening on port ${port}`);
     });
+    server.keepAliveTimeout = 120 * 1000;
+    server.headersTimeout = 120 * 1000;
   })
   .catch((err) => console.log(err));
